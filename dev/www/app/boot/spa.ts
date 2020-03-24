c80e988ea951f3e5d0330ecfd8cb0852a1864b00
Sumit Kumar Maitra <sumitkm@gmail.com>
2020-03-24 01:20:53 +0000

    RequirejS Loaded

---
+++ dev/www/app/boot/spa.ts
@@ -0,0 +1,9 @@
+export class Spa{
+    /**
+     *
+     */
+    constructor() {
+        
+    }
+    
+}
---
+++ dev/www/app/routing/router.ts
@@ -0,0 +1,8 @@
+export class Router{
+    /**
+     *
+     */
+    constructor() {
+        
+    }
+}
